a8b58fc0123fb944e233804cd0bf0305fd357a4b
Gaurzz <gauri9117@gmail.com>
2016-09-26 21:20:19 +0800

    Made Changes to FIND command so that the search becomes more efficient and not case sensitive

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Keyword matching is not case sensitive.
  */
 public class FindCommand extends Command {
 
@@ -34,19 +34,28 @@ public class FindCommand extends Command {
     public CommandResult execute() {
         final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
         return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
-    }
-
+    }    
+    
+    
     /**
      * Retrieve all persons in the address book whose names contain some of the specified keywords.
-     *
+     * Alters the keywords and the words in the name to lowerCase to ensure the command is not case sensitive 
      * @param keywords for searching
      * @return list of persons found
      */
     private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+        Set<String> keywords_lower = new HashSet();
+        for(String str : keywords){
+        	keywords_lower.add(str.toLowerCase());
+        }
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
             final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
-            if (!Collections.disjoint(wordsInName, keywords)) {
+            Set<String> wordsInName_lower = new HashSet();
+            for(String str : wordsInName){
+            	wordsInName_lower.add(str.toLowerCase());
+            }
+            if (!Collections.disjoint(wordsInName_lower, keywords_lower)) {
                 matchedPersons.add(person);
             }
         }
