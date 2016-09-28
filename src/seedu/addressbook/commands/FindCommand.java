209c923a6ada9de34236eb7b626b76f963b79353
Nathanael Chan <nathanaelchan94@gmail.com>
2016-09-28 23:26:57 +0800

    Improved the find command further to search through names, email, phone numbers and tags. Also disregards any duplicate persons that are being added to the list.

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -33,18 +33,35 @@ public class FindCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
+        final List<ReadOnlyPerson> personsFound = getPersonsWithInformationContainingAnyKeyword(keywords);
         return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
+    }
+    
+    /**
+     * Calls several methods to get names of all persons whose name, tags, email or phone contain any of the specified keywords
+     * 
+     * @param keywords
+     * @return matchedPersons
+     */
+    private List<ReadOnlyPerson> getPersonsWithInformationContainingAnyKeyword(Set<String> keywords){
+    	List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+    	
+    	matchedPersons = getPersonsWithNameContainingAnyKeyword(keywords, matchedPersons);
+    	matchedPersons = getPersonsWithTagsContainingAnyKeyword(keywords, matchedPersons);
+    	matchedPersons = getPersonsWithPhoneNumberContainingAnyKeywords(keywords, matchedPersons);
+    	matchedPersons = getPersonsWithEmailContainingAnyKeyword(keywords, matchedPersons);
+    	
+    	return matchedPersons;
     }
 
     /**
-     * Retrieve all persons and tags in the address book whose names or tags contain some of the specified keywords 
+     * Retrieve all persons in the address book whose names contain any of the specified keywords 
      *
      * @param keywords for searching
+     * @param matchedPersons
      * @return list of persons found
      */
-    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
-        final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords, List<ReadOnlyPerson> matchedPersons) {
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
             final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
             if (!Collections.disjoint(wordsInName, keywords)) {
@@ -52,13 +69,61 @@ public class FindCommand extends Command {
             }
         }
         
-        for(ReadOnlyPerson person : addressBook.getAllPersons()){
-        	if(!Collections.disjoint(person.getTags().getListOfTags(), keywords)){
+        return matchedPersons;
+    }
+
+	/**
+	 * Retrieve all persons in the address book whose tags are contained in any of the specified keywords
+	 * 
+	 * @param keywords
+	 * @param matchedPersons
+	 * @return list of persons found
+	 */
+	private List<ReadOnlyPerson> getPersonsWithTagsContainingAnyKeyword(Set<String> keywords, List<ReadOnlyPerson> matchedPersons) {
+		for(ReadOnlyPerson person : addressBook.getAllPersons()){
+        	if(!Collections.disjoint(person.getTags().getListOfTags(), keywords) && !matchedPersons.contains(person)){
         		matchedPersons.add(person);
         	}
         }
         
         return matchedPersons;
-    }
+	}
+	
+	/**
+	 * 
+	 * Retrieves all persons in the address book whose phone numbers are contained in any of the specified keywords
+	 * 
+	 * @param keywords
+	 * @param matchedPersons
+	 * @return
+	 */
+	private List<ReadOnlyPerson> getPersonsWithPhoneNumberContainingAnyKeywords(Set<String> keywords, List<ReadOnlyPerson> matchedPersons){
+		for(ReadOnlyPerson person: addressBook.getAllPersons()){
+			if(keywords.contains(person.getPhone().toString()) && !matchedPersons.contains(person)){
+				matchedPersons.add(person);
+			}
+		}
+		
+		
+		return matchedPersons;
+	}
+	
+	/**
+	 * 
+	 * Retrieves all persons in the address book whose emails are contained in any of the specified keywords
+	 * 
+	 * @param keywords
+	 * @param matchedPersons
+	 * @return
+	 */
+	private List<ReadOnlyPerson> getPersonsWithEmailContainingAnyKeyword(Set<String> keywords, List<ReadOnlyPerson> matchedPersons){
+		for(ReadOnlyPerson person: addressBook.getAllPersons()){
+			if(keywords.contains(person.getEmail().value) && !matchedPersons.contains(person)){
+				matchedPersons.add(person);
+			}
+		}
+		
+		return matchedPersons;
+	}
 
 }
